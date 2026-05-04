926b6201f6fe5f4406b980952249a20c65f54b33
beatriz-rios <beatriz.rios6@aluno.senai.br>
2026-05-04 11:17:39 -0300

    Update cadProd.php

--- php/cadProd.php
+++ php/cadProd.php
@@ -12,9 +12,46 @@
     <label for="nome">Nome do Produto:</label>
     <input type="text" id="nome" name="nome" required>
 
-  
+   <label for="marca">Nome da Marca:</label>
+   <input type="text" id="marca" name="marca" required>
+
+   <label for="volume">Digite o volume do produto:</label>
+   <input type="text" id="vol" name="vol" required>
+
+   <label for="cor">Digite a cor do produto:</label>
+   <input type="text" id="cor" name="cor" required>
+        
+        
 
     <input type="submit" value="Cadastrar">
     </form>
+
+    <?php 
+    if($_SERVER["REQUEST_METHO"]"POST){
+    $nome = $_POST['nome'];
+    $marca = $_POST['marca'];
+    $vol = $_POST['vol'];
+    $cor = $_POST['cor'];
+
+    $database = "lojaCosmi";
+    $password = "";
+    $servername = "localhost";
+    $username = "root";
+
+$conn = mysqli_connect($database, $password, $servername, $username );
+if(!conn){
+echo"<div class= 'message-erro'>"falha de conecção" . mysqli_connect_error() . "</div>";
+die();
+}
+sql = INSERT INTO cosmetico(
+    )VALUES(
+    
+    );
+
+
+    
+    }
+
+?>
 </body>
 </html>
